8d16a569f4c5ea38c2ee1d836603d92687abd753
Dione099 <dionegeorge.dg@gmail.com>
2024-12-04 03:48:16 +0000

    ran into problems going to fix errors

--- .ccls-cache/@home@runner@Chapter-5/main.cpp
+++ .ccls-cache/@home@runner@Chapter-5/main.cpp
@@ -20,3 +20,21 @@ do{
     cout<< "Error the average daily population cant be negative."<<endl;
   }
 }while(dailyIncrease<0);
+  //input number of days
+do{
+  cout<<"Enter the number of days they will multiply: ";
+  cin>>days;
+  if(days<1)
+    cout<< "Error the number of days must be at least 1."<<endl;
+}while(days<1);
+  //calculate population size for each day
+dailyIncrease/=100;
+cout<<"nDay\tPopulation"<<endl;"
+cout<<"-----------------"<<endl;"
+  //display population size for each day
+  double population=startingSize;
+for(int day=1;day<=days;++day)
+  cout<< day << "\t" << fixed << setprecision(2) << population << endl;
+population+=population*dailyIncrease;}
+return 0;
+}
