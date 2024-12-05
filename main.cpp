c71f6b8afb887e3de0ae21fdd28af0e4c7327def
Dione099 <dionegeorge.dg@gmail.com>
2024-12-05 15:05:35 +0000

    still having trouble with the program to read the file

--- main.cpp
+++ main.cpp
@@ -11,5 +11,36 @@ int main() {
     cout << "Enter the name of the data file: ";
     getline(cin, fileName);
 
-   
-        
+    cout << "Enter the name of the town: ";
+    getline(cin, townName);
+
+    cout<< "Enter the name of the data file: ";
+   getline(cin,fileName);
+
+    ifstream dataFile(fileName);
+
+    if (!dataFile){
+        cout << "Error opening file." << fileName << endl;
+        return 1;
+    }
+    // Print the header
+        cout << "\n" << townName << " Population Growth\n";
+        cout << "(Each * represents 1,000 people)\n\n";
+
+        // Read and process the file contents
+        while (dataFile >> year >> population) {
+            // Display the year
+            cout << year << " ";
+
+            // Display the bar chart
+            for (int i = 0; i < population / 1000; i++) {
+                cout << "*";
+            }
+            cout << endl; // Move to the next line
+        }
+
+        // Close the file
+        dataFile.close();
+
+        return 0;
+    }
